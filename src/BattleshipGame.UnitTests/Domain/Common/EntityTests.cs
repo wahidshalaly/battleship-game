7fb6a217692663615772fdd97125c0330f34079a
Wahid Shalaly <194151+wahidshalaly@users.noreply.github.com>
2025-06-25 12:17:53 +1000

    Enhance unit tests

---
+++ src/BattleshipGame.UnitTests/Domain/Common/EntityTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using BattleshipGame.Domain.Common;
+using FluentAssertions;
+using Xunit;
+
+namespace BattleshipGame.UnitTests.Domain.Common;
+
+public class EntityTests
+{
+    [Theory]
+    [InlineData(true, true)]
+    [InlineData(false, false)]
+    public void Equals_WhenSameInstanceOrNot(bool areSame, bool expectedResult)
+    {
+        var (subject, subject2) = CreateSubject(areSame);
+
+        subject.Equals(subject2).Should().Be(expectedResult);
+    }
+
+    [Fact]
+    public void GetHashCode_WhenSameInstance_ReturnsSameHashCode()
+    {
+        var (subject, subject2) = CreateSubject(areSame: true);
+
+        subject.GetHashCode().Should().Be(subject2.GetHashCode());
+    }
+
+    [Fact]
+    public void GetHashCode_WhenNotSameInstance_ReturnsDifferentHashCode()
+    {
+        var (subject, subject2) = CreateSubject(areSame: false);
+
+        subject.GetHashCode().Should().NotBe(subject2.GetHashCode());
+    }
+
+    private static (TestEntity, TestEntity) CreateSubject(bool areSame)
+    {
+        var id1 = Guid.NewGuid();
+        var id2 = areSame ? id1 : Guid.NewGuid();
+        var subject = new TestEntity(id1);
+        var subject2 = new TestEntity(id2);
+        return (subject, subject2);
+    }
+}
+
+internal class TestEntity : Entity<Guid>
+{
+    private static readonly Guid uuid = Guid.NewGuid();
+
+    public TestEntity(Guid id)
+    {
+        Id = id;
+    }
+
+    public Guid Value1 { get; } = uuid;
+    public int Value2 { get; } = Random.Shared.Next();
+    public string Value3 { get; } = uuid.ToString("N");
+    public DateTime Value4 { get; } = DateTime.UtcNow;
+    public object Value5 { get; } = new();
+}
--- src/BattleshipGame.UnitTests/Domain/Common/ValueObjectTests.cs
+++ src/BattleshipGame.UnitTests/Domain/Common/ValueObjectTests.cs
@@ -4,68 +4,64 @@ using BattleshipGame.Domain.Common;
 using FluentAssertions;
 using Xunit;
 
-namespace BattleshipGame.UnitTests.Domain.Common
+namespace BattleshipGame.UnitTests.Domain.Common;
+
+public class ValueObjectTests
 {
-    public class ValueObjectTests
+    [Theory]
+    [InlineData(true, true)]
+    [InlineData(false, false)]
+    public void Equals_WhenSameInstanceOrNot(bool areSame, bool expectedResult)
     {
-        [Theory]
-        [InlineData(true, true)]
-        [InlineData(false, false)]
-        public void Equals_WhenSameInstance_ReturnsTrue(bool areSame, bool expectedResult)
-        {
-            var (subject, subject2) = CreateSubject(areSame);
+        var (subject, subject2) = CreateSubject(areSame);
 
-            subject.Equals(subject2).Should().Be(expectedResult);
-        }
-
-        [Theory]
-        [InlineData(true, true)]
-        [InlineData(false, false)]
-        public void EqualOperator_WhenSameInstance_ReturnsTrue(bool areSame, bool expectedResult)
-        {
-            var (subject, subject2) = CreateSubject(areSame);
-
-            (subject == subject2).Should().Be(expectedResult);
-        }
-
-        [Theory]
-        [InlineData(true, false)]
-        [InlineData(false, true)]
-        public void NotEqualOperator_WhenSameInstance_ReturnsFalse(
-            bool areSame,
-            bool expectedResult
-        )
-        {
-            var (subject, subject2) = CreateSubject(areSame);
-
-            (subject != subject2).Should().Be(expectedResult);
-        }
-
-        private static (TestValueObject, TestValueObject) CreateSubject(bool areSame)
-        {
-            var subject = new TestValueObject();
-            var subject2 = areSame ? subject : new TestValueObject();
-            return (subject, subject2);
-        }
+        subject.Equals(subject2).Should().Be(expectedResult);
     }
 
-    internal class TestValueObject : ValueObject
+    [Theory]
+    [InlineData(true, true)]
+    [InlineData(false, false)]
+    public void EqualOperator_WhenSameInstanceOrNot(bool areSame, bool expectedResult)
     {
-        private static readonly Guid uuid = Guid.NewGuid();
+        var (subject, subject2) = CreateSubject(areSame);
 
-        public Guid Value1 { get; } = uuid;
-        public int Value2 { get; } = Random.Shared.Next();
-        public string Value3 { get; } = uuid.ToString("N");
-        public DateTime Value4 { get; } = DateTime.UtcNow;
-        public object Value5 { get; } = new();
+        (subject == subject2).Should().Be(expectedResult);
+    }
 
-        protected override IEnumerable<object> GetEqualityComponents()
-        {
-            yield return Value1;
-            yield return Value2;
-            yield return Value3;
-            yield return Value4;
-            yield return Value5;
-        }
+    [Theory]
+    [InlineData(true, false)]
+    [InlineData(false, true)]
+    public void NotEqualOperator_WhenSameInstanceOrNot(bool areSame, bool expectedResult)
+    {
+        var (subject, subject2) = CreateSubject(areSame);
+
+        (subject != subject2).Should().Be(expectedResult);
+    }
+
+    private static (TestValueObject, TestValueObject) CreateSubject(bool areSame)
+    {
+        var subject = new TestValueObject();
+        var subject2 = areSame ? subject : new TestValueObject();
+        return (subject, subject2);
+    }
+}
+
+internal class TestValueObject : ValueObject
+{
+    private static readonly Guid uuid = Guid.NewGuid();
+
+    public Guid Value1 { get; } = uuid;
+    public int Value2 { get; } = Random.Shared.Next();
+    public string Value3 { get; } = uuid.ToString("N");
+    public DateTime Value4 { get; } = DateTime.UtcNow;
+    public object Value5 { get; } = new();
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value1;
+        yield return Value2;
+        yield return Value3;
+        yield return Value4;
+        yield return Value5;
     }
 }
